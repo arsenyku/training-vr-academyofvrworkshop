9242a4b6a67f1690e71e10f50c338ec3c3f7e867
asu <arsenyk.ustaris.dev@gmail.com>
2017-05-06 15:48:16 -0700

    Play sound on click

--- Assets/Scripts/ChangeColour.cs
+++ Assets/Scripts/ChangeColour.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class ChangeColour : MonoBehaviour {
 
 	public MeshRenderer meshRenderer;
+	private AudioSource noiseMaker;
+
+	public AudioClip clip1;
+	public AudioClip clip2;
+	private bool switcher = false;
+
 
 	// Use this for initialization
 	void Start () {
-		
 		meshRenderer = this.GetComponent<MeshRenderer> ();
+		noiseMaker = this.GetComponent<AudioSource> ();
 	}
 
 	public void ChangeMyMaterial(Material newMaterial)
@@ -17,4 +23,11 @@ public class ChangeColour : MonoBehaviour {
 		meshRenderer.material = newMaterial;
 	}
 			
+
+	public void PlayAudioClip()
+	{
+		noiseMaker.clip = switcher ? clip2 : clip1;
+		switcher = !switcher;
+		noiseMaker.Play ();
+	}
 }
