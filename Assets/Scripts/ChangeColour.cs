a69ed2110add34fa4cc8ae245b55f2a20b93abac
asu <arsenyk.ustaris.dev@gmail.com>
2017-05-06 14:39:13 -0700

    Change colour of floor and cube when clicking on cube

---
+++ Assets/Scripts/ChangeColour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColour : MonoBehaviour {
+
+	public Color newColour;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void ChangeMyColour()
+	{
+		this.GetComponent<MeshRenderer> ().material.color = newColour;
+	}
+			
+}
